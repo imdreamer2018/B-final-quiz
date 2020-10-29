1c4880053aad49a0a60ee80eb9d540a2c0518b1e
yangqian <743295483@qq.com>
2020-10-29 13:43:56 +0800

    [yangqian] feat: trainee repository find ungrouped trainees JPA test.

--- src/test/java/com/example/demo/repositoryTests/TraineeRepositoryTest.java
+++ src/test/java/com/example/demo/repositoryTests/TraineeRepositoryTest.java
@@ -1,17 +1,45 @@
 package com.example.demo.repositoryTests;
 
+import com.example.demo.entity.TraineeEntity;
 import com.example.demo.repository.TraineeRepository;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
 import org.springframework.boot.test.autoconfigure.orm.jpa.TestEntityManager;
 
-@AutoConfigureTestDatabase(replace = AutoConfigureTestDatabase.Replace.NONE)
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
 @DataJpaTest
-public class TraineeRepositoryTest {
+class TraineeRepositoryTest {
 
     @Autowired
     private TraineeRepository traineeRepository;
     @Autowired
     private TestEntityManager entityManager;
+
+    private TraineeEntity traineeEntity;
+
+
+    @BeforeEach
+    void setUp() {
+        traineeEntity = TraineeEntity.builder()
+                .name("mock name")
+                .grouped(false)
+                .build();
+    }
+
+    @Test
+    void should_return_trainees_info_when_find_ungrouped_trainees() {
+
+        entityManager.persistAndFlush(traineeEntity);
+
+        List<TraineeEntity> trainees = traineeRepository.findAllAndGroupedIsFalse(0);
+
+        assertEquals("mock name", trainees.get(0).getName());
+    }
+
 }
+

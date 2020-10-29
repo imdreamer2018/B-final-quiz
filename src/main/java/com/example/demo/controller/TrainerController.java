e3f0bf2901567e28233ca5581a676eb0e3165547
yangqian <743295483@qq.com>
2020-10-29 14:37:54 +0800

    [yangqian] feat: create trainer controller and test.

--- src/main/java/com/example/demo/controller/TrainerController.java
+++ src/main/java/com/example/demo/controller/TrainerController.java
@@ -1,7 +1,12 @@
 package com.example.demo.controller;
 
+import com.example.demo.dto.Trainer;
 import com.example.demo.service.TrainerService;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -12,5 +17,11 @@ public class TrainerController {
 
     public TrainerController(TrainerService trainerService) {
         this.trainerService = trainerService;
+    }
+
+    @PostMapping
+    @ResponseStatus(HttpStatus.CREATED)
+    public Trainer createTrainer(@RequestBody Trainer trainer) {
+        return trainerService.createTrainer(trainer);
     }
 }
--- src/test/java/com/example/demo/controllerTests/TrainerControllerTest.java
+++ src/test/java/com/example/demo/controllerTests/TrainerControllerTest.java
@@ -5,13 +5,23 @@ import com.example.demo.dto.Trainer;
 import com.example.demo.service.TrainerService;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Nested;
+import org.junit.jupiter.api.Test;
 import org.mockito.Mockito;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.json.AutoConfigureJsonTesters;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.boot.test.json.JacksonTester;
 import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.http.MediaType;
 import org.springframework.test.web.servlet.MockMvc;
+
+import static org.hamcrest.Matchers.is;
+import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.when;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 @WebMvcTest(TrainerController.class)
 @AutoConfigureJsonTesters
@@ -39,5 +49,20 @@ public class TrainerControllerTest {
     @AfterEach
     void afterEach() {
         Mockito.reset(trainerService);
+    }
+
+    @Nested
+    class CreateTrainee {
+
+        @Test
+        void should_return_trainee_info() throws Exception {
+            when(trainerService.createTrainer(trainer)).thenReturn(trainer);
+            mockMvc.perform(post("/trainers")
+                    .content(trainerJson.write(trainer).getJson())
+                    .contentType(MediaType.APPLICATION_JSON))
+                    .andExpect(status().isCreated())
+                    .andExpect(jsonPath("$.name", is("mock trainer name")));
+            verify(trainerService).createTrainer(trainer);
+        }
     }
 }

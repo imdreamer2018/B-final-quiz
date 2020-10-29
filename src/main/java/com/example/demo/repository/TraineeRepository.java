9469dd4995e0fda6ee36a2cbb2b8026141b486e7
yangqian <743295483@qq.com>
2020-10-29 13:10:01 +0800

    [yangqian] feat: add find all trainees and grouped is false.

--- src/main/java/com/example/demo/repository/TraineeRepository.java
+++ src/main/java/com/example/demo/repository/TraineeRepository.java
@@ -2,6 +2,13 @@ package com.example.demo.repository;
 
 import com.example.demo.entity.TraineeEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+
+import java.util.List;
 
 public interface TraineeRepository extends JpaRepository<TraineeEntity, Long> {
+
+    @Query(nativeQuery = true, value = "select * from trainee t where t.grouped= :grouped")
+    List<TraineeEntity> findAllAndGroupedIsFalse(@Param("grouped") int grouped);
 }

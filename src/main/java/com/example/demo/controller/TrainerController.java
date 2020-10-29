f94fed81304ca1d8683002cfe43bb701afddad99
yangqian <743295483@qq.com>
2020-10-29 15:02:49 +0800

    [yangqian] feat: delete trainer controller.

--- src/main/java/com/example/demo/controller/TrainerController.java
+++ src/main/java/com/example/demo/controller/TrainerController.java
@@ -3,7 +3,9 @@ package com.example.demo.controller;
 import com.example.demo.dto.Trainer;
 import com.example.demo.service.TrainerService;
 import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -32,5 +34,11 @@ public class TrainerController {
     @GetMapping
     public List<Trainer> getTrainers(@RequestParam(required = false) boolean grouped) {
         return trainerService.getTrainers(grouped);
+    }
+
+    @DeleteMapping("/{id}")
+    @ResponseStatus(HttpStatus.NO_CONTENT)
+    public void deleteTrainer(@PathVariable(name = "id") Long trainerId) {
+        trainerService.deleteTrainer(trainerId);
     }
 }

a1050584e34314599dc4c89eece55af86cd6ba60
yangqian <743295483@qq.com>
2020-10-29 12:26:13 +0800

    [yangqian] feat: create trainee service and test.

--- src/main/java/com/example/demo/service/TraineeService.java
+++ src/main/java/com/example/demo/service/TraineeService.java
@@ -1,5 +1,7 @@
 package com.example.demo.service;
 
+import com.example.demo.dto.Trainee;
+import com.example.demo.entity.TraineeEntity;
 import com.example.demo.repository.TraineeRepository;
 import org.springframework.stereotype.Service;
 
@@ -10,5 +12,12 @@ public class TraineeService {
 
     public TraineeService(TraineeRepository traineeRepository) {
         this.traineeRepository = traineeRepository;
+    }
+
+    public Trainee createTrainee(Trainee trainee) {
+        TraineeEntity traineeEntity = Trainee.toTraineeEntity(trainee);
+        traineeRepository.save(traineeEntity);
+        trainee.setId(traineeEntity.getId());
+        return trainee;
     }
 }
--- src/test/java/com/example/demo/serviceTests/TraineeServiceTest.java
+++ src/test/java/com/example/demo/serviceTests/TraineeServiceTest.java
@@ -1,12 +1,16 @@
 package com.example.demo.serviceTests;
 
+import com.example.demo.dto.Trainee;
 import com.example.demo.repository.TraineeRepository;
 import com.example.demo.service.TraineeService;
 import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Nested;
+import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.mockito.MockitoAnnotations.initMocks;
 
 @ExtendWith(MockitoExtension.class)
@@ -17,10 +21,27 @@ public class TraineeServiceTest {
     @Mock
     TraineeRepository traineeRepository;
 
+    private Trainee trainee;
+
     @BeforeEach
     void setUp() {
         initMocks(this);
         traineeService = new TraineeService(traineeRepository);
+        trainee = Trainee.builder()
+                .name("mock name")
+                .build();
     }
+
+    @Nested
+    class CreateTrainee {
+
+            @Test
+            void shouldReturnTraineeInfo() {
+                Trainee traineeResponse = traineeService.createTrainee(trainee);
+                assertEquals("mock name", traineeResponse.getName());
+            }
+
+    }
+
 
 }

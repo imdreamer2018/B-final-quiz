f03f8ab373fc315ac8cdb3dba2174daa770127e2
yangqian <743295483@qq.com>
2020-10-29 16:57:22 +0800

    [yangqian] feat: auto grouping service and test.

--- src/main/java/com/example/demo/service/GroupService.java
+++ src/main/java/com/example/demo/service/GroupService.java
@@ -1,14 +1,75 @@
 package com.example.demo.service;
 
+import com.example.demo.dto.Group;
+import com.example.demo.entity.GroupEntity;
+import com.example.demo.entity.TraineeEntity;
+import com.example.demo.entity.TrainerEntity;
 import com.example.demo.repository.GroupRepository;
+import com.example.demo.repository.TraineeRepository;
+import com.example.demo.repository.TrainerRepository;
 import org.springframework.stereotype.Service;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+import java.util.concurrent.atomic.AtomicLong;
+import java.util.stream.Collectors;
 
 @Service
 public class GroupService {
 
-    private final GroupRepository groupRepository;
+    private static final String GROUP_NAME = "组";
 
-    public GroupService(GroupRepository groupRepository) {
+    private final GroupRepository groupRepository;
+    private final TraineeRepository traineeRepository;
+    private final TrainerRepository trainerRepository;
+
+    public GroupService(GroupRepository groupRepository, TraineeRepository traineeRepository, TrainerRepository trainerRepository) {
         this.groupRepository = groupRepository;
+        this.traineeRepository = traineeRepository;
+        this.trainerRepository = trainerRepository;
     }
+
+    public List<Group> autoGrouping() {
+        groupRepository.deleteAll();
+        List<TraineeEntity> traineeEntities = traineeRepository.findAllAndGroupedIsFalse(0);
+        List<TrainerEntity> trainerEntities = trainerRepository.findAllAndGroupedIsFalse(0);
+        Collections.shuffle(traineeEntities);
+        Collections.shuffle(trainerEntities);
+
+        List<GroupEntity> groupEntities = new ArrayList<>();
+        AtomicLong groupIndex = new AtomicLong(0L);
+        int groupNum = trainerEntities.size() / 2;
+
+        for (TraineeEntity traineeEntity: traineeEntities) {
+            List<TrainerEntity> trainers = new ArrayList<>();
+            List<TraineeEntity> trainees = new ArrayList<>();
+            GroupEntity groupEntity = new GroupEntity(groupIndex.get(), groupIndex.get() + GROUP_NAME, trainers, trainees);
+            if (groupEntities.size() < groupNum) {
+                groupEntities.add(groupEntity);
+            }
+            traineeEntity.setGrouped(true);
+            traineeRepository.save(traineeEntity);
+            groupEntities.get(Math.toIntExact(groupIndex.get())).setId(groupIndex.get() + 1);
+            groupEntities.get(Math.toIntExact(groupIndex.get())).setName(groupIndex.get() + 1 + GROUP_NAME);
+            groupEntities.get(Math.toIntExact(groupIndex.get())).getTraineeEntities().add(traineeEntity);
+            groupIndex.incrementAndGet();
+            if (groupIndex.get() == groupNum)
+                groupIndex.set(0L);
+        }
+
+        for (int i = 0; i < trainerEntities.size(); i += 2) {
+            trainerEntities.get(i).setGrouped(true);
+            trainerRepository.save(trainerEntities.get(i));
+            trainerEntities.get(i + 1).setGrouped(true);
+            trainerRepository.save(trainerEntities.get(i + 1));
+            groupEntities.get(i / 2).getTrainerEntities().add(trainerEntities.get(i));
+            groupEntities.get(i / 2).getTrainerEntities().add(trainerEntities.get(i + 1));
+        }
+
+        groupRepository.saveAll(groupEntities);
+        return groupEntities.stream()
+                .map(GroupEntity::toGroup).collect(Collectors.toList());
+    }
+
 }
--- src/test/java/com/example/demo/serviceTests/GroupServiceTest.java
+++ src/test/java/com/example/demo/serviceTests/GroupServiceTest.java
@@ -1,12 +1,24 @@
 package com.example.demo.serviceTests;
 
+import com.example.demo.dto.Group;
+import com.example.demo.entity.TraineeEntity;
+import com.example.demo.entity.TrainerEntity;
 import com.example.demo.repository.GroupRepository;
+import com.example.demo.repository.TraineeRepository;
+import com.example.demo.repository.TrainerRepository;
 import com.example.demo.service.GroupService;
 import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Nested;
+import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
 
+import java.util.ArrayList;
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.mockito.Mockito.when;
 import static org.mockito.MockitoAnnotations.initMocks;
 
 @ExtendWith(MockitoExtension.class)
@@ -17,9 +29,44 @@ public class GroupServiceTest {
     @Mock
     GroupRepository groupRepository;
 
+    @Mock
+    TrainerRepository trainerRepository;
+
+    @Mock
+    TraineeRepository traineeRepository;
+
+    private List<TraineeEntity> traineeEntities = new ArrayList<>();
+
+    private List<TrainerEntity> trainerEntities = new ArrayList<>();
+
     @BeforeEach
     void setUp() {
         initMocks(this);
-        groupService = new GroupService(groupRepository);
+        groupService = new GroupService(groupRepository, traineeRepository, trainerRepository);
+
+        for (int i = 0; i < 8; i++) {
+            traineeEntities.add(TraineeEntity.builder()
+                    .name("mock name"+ i)
+                    .grouped(false)
+                    .build());
+        }
+        for (int i = 0; i < 8; i++) {
+            trainerEntities.add(TrainerEntity.builder()
+                    .name("mock trainer name"+ i)
+                    .grouped(false)
+                    .build());
+        }
+    }
+
+    @Nested
+    class CreateGroups {
+
+        @Test
+        void should_return_groups_info() {
+            when(traineeRepository.findAllAndGroupedIsFalse(0)).thenReturn(traineeEntities);
+            when(trainerRepository.findAllAndGroupedIsFalse(0)).thenReturn(trainerEntities);
+            List<Group> groups = groupService.autoGrouping();
+            assertEquals(4, groups.size());
+        }
     }
 }

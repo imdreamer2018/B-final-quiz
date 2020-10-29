ddb2eb53ab9551fcf374a9ee6c023237235d05fa
yangqian <743295483@qq.com>
2020-10-29 21:02:37 +0800

    [yangqian] fix: add validated.

--- src/main/java/com/example/demo/controller/TrainerController.java
+++ src/main/java/com/example/demo/controller/TrainerController.java
@@ -3,6 +3,7 @@ package com.example.demo.controller;
 import com.example.demo.dto.Trainer;
 import com.example.demo.service.TrainerService;
 import org.springframework.http.HttpStatus;
+import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -19,6 +20,7 @@ import javax.validation.constraints.NotNull;
 import java.util.List;
 
 @RestController
+@Validated
 @RequestMapping("/trainers")
 public class TrainerController {
 

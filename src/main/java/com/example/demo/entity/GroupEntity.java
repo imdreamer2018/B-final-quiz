9298508a689416b39a01efbc26f22e4debd0dd42
yangqian <743295483@qq.com>
2020-10-29 16:57:04 +0800

    [yangqian] feat: add groupEntity to group function.

--- src/main/java/com/example/demo/entity/GroupEntity.java
+++ src/main/java/com/example/demo/entity/GroupEntity.java
@@ -1,5 +1,6 @@
 package com.example.demo.entity;
 
+import com.example.demo.dto.Group;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
@@ -11,6 +12,7 @@ import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.Table;
 import java.util.List;
+import java.util.stream.Collectors;
 
 @Data
 @AllArgsConstructor
@@ -26,4 +28,14 @@ public class GroupEntity {
     private String name;
     private List<TrainerEntity> trainerEntities;
     private List<TraineeEntity> traineeEntities;
+
+    public static Group toGroup(GroupEntity groupEntity) {
+
+        return Group.builder()
+                .id(groupEntity.getId())
+                .name(groupEntity.getName())
+                .trainers(groupEntity.getTrainerEntities().stream().map(TrainerEntity::toTrainer).collect(Collectors.toList()))
+                .trainees(groupEntity.getTraineeEntities().stream().map(TraineeEntity::toTrainee).collect(Collectors.toList()))
+                .build();
+    }
 }

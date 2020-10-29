65acb331d42eace01a65f83f8147668943681ae1
yangqian <743295483@qq.com>
2020-10-29 17:51:46 +0800

    [yangqian] feat: add one to many.

--- src/main/java/com/example/demo/entity/GroupEntity.java
+++ src/main/java/com/example/demo/entity/GroupEntity.java
@@ -10,6 +10,7 @@ import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.OneToMany;
 import javax.persistence.Table;
 import java.util.List;
 import java.util.stream.Collectors;
@@ -19,23 +20,26 @@ import java.util.stream.Collectors;
 @NoArgsConstructor
 @Builder
 @Entity
-@Table(name = "group")
+@Table(name = "train_group")
 public class GroupEntity {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
     private String name;
-    private List<TrainerEntity> trainerEntities;
-    private List<TraineeEntity> traineeEntities;
+
+    @OneToMany
+    private List<TrainerEntity> trainers;
+    @OneToMany
+    private List<TraineeEntity> trainees;
 
     public static Group toGroup(GroupEntity groupEntity) {
 
         return Group.builder()
                 .id(groupEntity.getId())
                 .name(groupEntity.getName())
-                .trainers(groupEntity.getTrainerEntities().stream().map(TrainerEntity::toTrainer).collect(Collectors.toList()))
-                .trainees(groupEntity.getTraineeEntities().stream().map(TraineeEntity::toTrainee).collect(Collectors.toList()))
+                .trainers(groupEntity.getTrainers().stream().map(TrainerEntity::toTrainer).collect(Collectors.toList()))
+                .trainees(groupEntity.getTrainees().stream().map(TraineeEntity::toTrainee).collect(Collectors.toList()))
                 .build();
     }
 }

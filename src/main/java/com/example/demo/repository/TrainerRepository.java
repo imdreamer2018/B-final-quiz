1cd9877ed90d5eb29cc416d420cff536cb9bb4bd
yangqian <743295483@qq.com>
2020-10-29 14:54:42 +0800

    [yangqian] feat: find trainers and grouped is false in trainer repository.

--- src/main/java/com/example/demo/repository/TrainerRepository.java
+++ src/main/java/com/example/demo/repository/TrainerRepository.java
@@ -2,6 +2,12 @@ package com.example.demo.repository;
 
 import com.example.demo.entity.TrainerEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+
+import java.util.List;
 
 public interface TrainerRepository extends JpaRepository<TrainerEntity, Long> {
+    @Query(nativeQuery = true, value = "select * from trainer t where t.grouped= :grouped")
+    List<TrainerEntity> findAllAndGroupedIsFalse(@Param("grouped") int grouped);
 }

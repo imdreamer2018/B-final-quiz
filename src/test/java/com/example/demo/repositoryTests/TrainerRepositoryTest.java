f92a8c09e2f5d9441b3ea65da37e380d3a1581ba
yangqian <743295483@qq.com>
2020-10-29 15:00:53 +0800

    [yangqian] feat: find ungrouped trainers JPA test.

--- src/test/java/com/example/demo/repositoryTests/TrainerRepositoryTest.java
+++ src/test/java/com/example/demo/repositoryTests/TrainerRepositoryTest.java
@@ -3,12 +3,17 @@ package com.example.demo.repositoryTests;
 import com.example.demo.entity.TrainerEntity;
 import com.example.demo.repository.TrainerRepository;
 import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
 import org.springframework.boot.test.autoconfigure.orm.jpa.TestEntityManager;
 
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
 @DataJpaTest
-public class TrainerRepositoryTest {
+class TrainerRepositoryTest {
 
     @Autowired
     private TrainerRepository trainerRepository;
@@ -24,5 +29,15 @@ public class TrainerRepositoryTest {
                 .name("mock trainer name")
                 .grouped(false)
                 .build();
+    }
+
+    @Test
+    void should_return_trainees_info_when_find_ungrouped_trainees() {
+
+        entityManager.persistAndFlush(trainerEntity);
+
+        List<TrainerEntity> trainers = trainerRepository.findAllAndGroupedIsFalse(0);
+
+        assertEquals("mock trainer name", trainers.get(0).getName());
     }
 }

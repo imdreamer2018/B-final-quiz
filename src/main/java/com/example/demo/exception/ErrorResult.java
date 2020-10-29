8074fd7378787803db784954b634b4bbf4612226
yangqian <743295483@qq.com>
2020-10-29 14:16:42 +0800

    [yangqian] feat: add global exception handler and error dot and resource not found exception.

---
+++ src/main/java/com/example/demo/exception/ErrorResult.java
@@ -0,0 +1,14 @@
+package com.example.demo.exception;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+class ErrorResult {
+    private String message;
+}
---
+++ src/main/java/com/example/demo/exception/GlobalExceptionHandler.java
@@ -0,0 +1,48 @@
+package com.example.demo.exception;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.MethodArgumentNotValidException;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+
+import javax.validation.ConstraintViolation;
+import javax.validation.ConstraintViolationException;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Objects;
+
+@ControllerAdvice
+public class GlobalExceptionHandler {
+
+
+    @ExceptionHandler(ResourceNotFoundException.class)
+    public ResponseEntity<ErrorResult> handle(ResourceNotFoundException e) {
+        ErrorResult errorResult = ErrorResult.builder()
+                .message(e.getMessage())
+                .build();
+        return ResponseEntity.status(HttpStatus.NOT_FOUND).body(errorResult);
+    }
+
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    public ResponseEntity<ErrorResult> handle(MethodArgumentNotValidException e) {
+        ErrorResult errorResult = ErrorResult.builder()
+                .message(Objects.requireNonNull(e.getBindingResult().getFieldError()).getDefaultMessage())
+                .build();
+        return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(errorResult);
+    }
+
+    @ExceptionHandler(ConstraintViolationException.class)
+    public ResponseEntity<ErrorResult> handle(ConstraintViolationException e) {
+        List<String> messageList = new ArrayList<>();
+        for (ConstraintViolation<?> constraint : e.getConstraintViolations()) {
+            messageList.add(constraint.getMessage());
+        }
+        ErrorResult errorResult = ErrorResult.builder()
+                .message(messageList.toString())
+                .build();
+        return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(errorResult);
+    }
+
+
+}

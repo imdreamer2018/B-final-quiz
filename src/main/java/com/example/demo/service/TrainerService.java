346606ef586a8464562cd3d9123d8d281f10591f
yangqian <743295483@qq.com>
2020-10-29 14:55:05 +0800

    [yangqian] feat: find trainers with grouped is false service and test.

--- src/main/java/com/example/demo/service/TrainerService.java
+++ src/main/java/com/example/demo/service/TrainerService.java
@@ -5,6 +5,10 @@ import com.example.demo.entity.TrainerEntity;
 import com.example.demo.repository.TrainerRepository;
 import org.springframework.stereotype.Service;
 
+import java.util.ArrayList;
+import java.util.List;
+import java.util.stream.Collectors;
+
 @Service
 public class TrainerService {
 
@@ -18,5 +22,14 @@ public class TrainerService {
         TrainerEntity trainerEntity = Trainer.toTrainerEntity(trainer);
         trainerRepository.save(trainerEntity);
         return TrainerEntity.toTrainer(trainerEntity);
+    }
+
+    public List<Trainer> getTrainers(boolean grouped) {
+        List<TrainerEntity> trainers = new ArrayList<>();
+        if (!grouped) {
+            trainers = trainerRepository.findAllAndGroupedIsFalse(0);
+        }
+        return trainers.stream()
+                .map(TrainerEntity::toTrainer).collect(Collectors.toList());
     }
 }
--- src/test/java/com/example/demo/serviceTests/TrainerServiceTest.java
+++ src/test/java/com/example/demo/serviceTests/TrainerServiceTest.java
@@ -11,7 +11,11 @@ import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.mockito.Mockito.when;
 import static org.mockito.MockitoAnnotations.initMocks;
 
 @ExtendWith(MockitoExtension.class)
@@ -43,6 +47,23 @@ public class TrainerServiceTest {
         void should_return_trainer_info() {
             Trainer trainerResponse = trainerService.createTrainer(trainer);
             assertEquals("mock trainer name", trainerResponse.getName());
+        }
+    }
+
+    @Nested
+    class FindTrainers {
+
+        @Nested
+        class WhenGroupedIsFalse {
+
+            @Test
+            void should_return_trainees_info() {
+                List<TrainerEntity> trainers = new ArrayList<>();
+                trainers.add(trainerEntity);
+                when(trainerRepository.findAllAndGroupedIsFalse(0)).thenReturn(trainers);
+                List<Trainer> trainersResponse = trainerService.getTrainers(false);
+                assertEquals("mock trainer name", trainersResponse.get(0).getName());
+            }
         }
     }
 

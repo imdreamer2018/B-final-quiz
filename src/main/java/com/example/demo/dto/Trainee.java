749477d04c23b1ef086042387cfa59e8e829ddee
yangqian <743295483@qq.com>
2020-10-29 11:50:03 +0800

    [yangqian] feat: add column data.

--- src/main/java/com/example/demo/dto/Trainee.java
+++ src/main/java/com/example/demo/dto/Trainee.java
@@ -5,9 +5,16 @@ import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
+import javax.validation.constraints.NotNull;
+
 @Data
 @AllArgsConstructor
 @NoArgsConstructor
 @Builder
 public class Trainee {
+
+    private Long id;
+    @NotNull
+    private String name;
+    private boolean grouped = false;
 }
--- src/main/java/com/example/demo/entity/TraineeEntity.java
+++ src/main/java/com/example/demo/entity/TraineeEntity.java
@@ -4,8 +4,12 @@ import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
+import org.hibernate.annotations.DynamicInsert;
 
 import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
 import javax.persistence.Table;
 
 @Data
@@ -15,4 +19,12 @@ import javax.persistence.Table;
 @Entity
 @Table(name = "trainee")
 public class TraineeEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String name;
+
+    private Boolean grouped;
 }
